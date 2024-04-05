af171778d03e58076ee069e05b1d58197cd343e0
Bigfab <oresanwoodunayo@gmail.com>
2024-04-05 17:59:57 +0100

    new commit

---
+++ src/Players.js
@@ -0,0 +1,36 @@
+const players = [
+    {
+        name: "Bruno Fernandes",
+        team: "Manchester United",
+        nationality: "Portugar",
+        jerseyNumber: 8,
+        age: "29 years",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/e/ee/Bruno_Fernandes_Portugal%2C_2018.jpg",
+    },
+    {
+        name: "Marcus Rashford",
+        team: "Manchester United",
+        nationality: "Englad",
+        jerseyNumber: 10,
+        age:  "26 years",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Marcus_Rashford%2C_FWC_2018_-_Round_of_16_-_COL_v_ENG_-_Photo_106_%28cropped%29.jpg",
+    },
+    {
+        name: "Kobbie Mainoo",
+        team: "Manchester United",
+        nationality: "England",
+        jerseyNumber: 37,
+        age:  "18 years",
+        imageUrl: "https://cdn1.unitedinfocus.com/uploads/14/2023/06/GettyImages-1467642124-1536x1025.jpg",
+    },
+    {
+        name: "Alejandro Garnacho",
+        team: "Manchester United",
+        nationality: "England",
+        jerseyNumber: 17,
+        age:  "24 years",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Alejandro_Garnacho_7_August_2022_%28cropped%29.jpg",
+    },
+    ];
+    
+    export default players;
